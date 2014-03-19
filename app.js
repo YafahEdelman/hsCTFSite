70410f48519c8884e18c712d887e60e7236d0ade
Ben Edelman <benedelman100@gmail.com>
2014-03-18 22:45:05 -0400

    cleaned stuff

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
+var logfmt = require('logfmt');
 var storage = require('node-persist');
 var path = require('path');
+
 storage.initSync();
+
 var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.configure(function(){
   app.set('views',  __dirname +'/views');//this may not work
   app.engine('html', require('ejs').renderFile);
+  app.use(express.static(path.join(__dirname, 'public')));
   app.use(logfmt.requestLogger());//is this really needed?
   app.use(express.cookieParser());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.session({ secret: 'keyboard cat' }));//what is this??
+  app.use(express.session({ secret: 'NoFmfULcyI5NyWtCYnKqdf5ry2sBodHr' }));
   app.use(app.router);
 });
 
@@ -21,14 +23,14 @@ app.configure(function(){
 app.get('/', function(req, res) {
   res.render('index.html');
 });
-app.post('/subscribe', function(req, res) {
-  var emails=storage.getItem("emails");
-  emails.push(req.body.email);
-  storage.setItem("emails",emails);
-});
 
+app.post('/subscribe', function(req, res) {
+  var emails=storage.getItem('emails');
+  emails.push(req.body.email);
+  storage.setItem('emails',emails);
+});
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on ' + port);
 });
